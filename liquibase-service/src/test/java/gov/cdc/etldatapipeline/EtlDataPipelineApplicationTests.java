9a0015d7e74a49a19f2dc483b3ced3cff9022339
Anand Logan <117679402+alog-enquizit@users.noreply.github.com>
2024-07-24 15:38:54 -0700

    CNDIT-1589: Liquibase migration to the NEDSS-DataReporting repository (#7)

---
+++ liquibase-service/src/test/java/gov/cdc/etldatapipeline/EtlDataPipelineApplicationTests.java
@@ -0,0 +1,12 @@
+package gov.cdc.etldatapipeline;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EtlDataPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true, "Unit Tests sanity check.");
+    }
+}
