14a712969e09f67dc297a61dbdafa7af124616af
Upasana Pattnaik <129798209+upasanapattnaik-eq@users.noreply.github.com>
2024-09-09 18:03:12 -0400

    Check for nrt-investigation-notification (#34)

--- liquibase-service/src/main/resources/db/rdb/tables/012-create-nrt-investigation-notification-002.sql
+++ liquibase-service/src/main/resources/db/rdb/tables/012-create-nrt-investigation-notification-002.sql
@@ -1,4 +1,7 @@
 IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_notifications' and xtype = 'U')
-BEGIN
-EXEC sys.sp_rename N'nrt_notifications', N'nrt_investigation_notification', 'OBJECT';
-END
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_investigation_notification' and xtype = 'U')
+            BEGIN
+                EXEC sys.sp_rename N'nrt_notifications', N'nrt_investigation_notification', 'OBJECT';
+            END
+    END
