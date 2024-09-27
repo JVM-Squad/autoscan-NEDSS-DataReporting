06ec34b3d27423fdfc414ad0537b04db89e69eb5
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-09-26 20:09:23 -0400

    more modification

--- liquibase-service/src/main/resources/db/rdb/tables/001-create_data_sync_config-001.sql
+++ liquibase-service/src/main/resources/db/rdb/tables/001-create_data_sync_config-001.sql
@@ -9,10 +9,10 @@ CREATE TABLE data_sync_config
     table_name                 NVARCHAR(255) NOT NULL PRIMARY KEY,
     source_db                  NVARCHAR(255) NOT NULL,
     query                      NVARCHAR( MAX) NOT NULL,
-    query_with_limit           NVARCHAR( MAX) NULL,
+    query_count                NVARCHAR( MAX) NOT NULL,
+    query_with_pagination      NVARCHAR( MAX) NOT NULL,
     query_with_null_timestamp  NVARCHAR( MAX) NULL,
     created_at                 DATETIME2 DEFAULT GETDATE(),
-    updated_at                 DATETIME2 DEFAULT GETDATE(),
     last_executed_timestamp    DATETIME2 NULL DEFAULT NULL,
     last_executed_run_time     NVARCHAR(255) NULL DEFAULT NULL,
     last_executed_result_count INTEGER NULL DEFAULT NULL
