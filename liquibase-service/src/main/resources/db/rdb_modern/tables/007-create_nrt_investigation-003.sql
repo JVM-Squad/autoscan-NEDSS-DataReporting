a189ff7b0d3dd614f1368278097aeee23b8fdd4e
Sergei Veselev <sveselev@enquizit.com>
2024-11-12 19:19:11 -0500

    CNDE-1602: Minor but important SQL fix (#79)

--- liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-003.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-003.sql
@@ -6,12 +6,12 @@ IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_investigation' and xtype =
                 ALTER TABLE dbo.nrt_investigation ADD investigation_count bigint;
             END;
 
-        IF EXISTS(SELECT 1 FROM sys.columns WHERE name = N'case_count' AND Object_ID = Object_ID(N'nrt_investigation'))
+        IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE name = N'case_count' AND Object_ID = Object_ID(N'nrt_investigation'))
             BEGIN
                 ALTER TABLE dbo.nrt_investigation ADD case_count bigint;
             END;
 
-        IF EXISTS(SELECT 1 FROM sys.columns WHERE name = N'investigator_assigned_datetime' AND Object_ID = Object_ID(N'nrt_investigation'))
+        IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE name = N'investigator_assigned_datetime' AND Object_ID = Object_ID(N'nrt_investigation'))
             BEGIN
                 ALTER TABLE dbo.nrt_investigation ADD investigator_assigned_datetime datetime;
             END;
