d9fc0552ab2c6bb98e15824ffbd7caedc4bdeec9
Upasana Pattnaik <129798209+upasanapattnaik-eq@users.noreply.github.com>
2024-11-26 16:45:14 -0500

    Liquibase fix (#98)

--- liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/007-create_nrt_investigation-001.sql
@@ -113,3 +113,13 @@ CREATE TABLE dbo.nrt_investigation (
     max_datetime                  datetime2(7) GENERATED ALWAYS AS ROW END HIDDEN NOT NULL,
     PERIOD FOR SYSTEM_TIME (refresh_datetime, max_datetime)
 );
+
+IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_investigation' and xtype = 'U')
+BEGIN
+
+        IF NOT EXISTS(SELECT 1 FROM sys.columns WHERE name = N'investigation_form_cd' AND Object_ID = Object_ID(N'nrt_investigation'))
+            BEGIN
+            ALTER TABLE dbo.nrt_investigation ADD investigation_form_cd VARCHAR(50);
+            END;
+
+END;
