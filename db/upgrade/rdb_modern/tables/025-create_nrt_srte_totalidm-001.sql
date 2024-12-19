52b96a5061d272a8540a0c8b25c29c94db1582e6
Ravindran Seshadri <rseshadri@enquizit.com>
2024-12-19 16:40:22 -0500

    NRT SRTE table scripts for consolidation

---
+++ db/upgrade/rdb_modern/tables/025-create_nrt_srte_totalidm-001.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dbo.nrt_srte_TotalIDM (
+	TotalIDM_id int IDENTITY(1,1) NOT NULL,
+	unique_cd nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SRT_reference nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	format nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	label nvarchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK_TotalIDM PRIMARY KEY (TotalIDM_id)
+);
+
