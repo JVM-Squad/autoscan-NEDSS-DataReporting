6f3a025597af1b7474126db6a781f9e96e4d944c
Ravindran Seshadri <109151953+rseshadri99@users.noreply.github.com>
2024-12-20 11:07:21 -0500

    Creation of srte tables as view and codeset/code_value_general Views (#118)

---
+++ liquibase-service/src/main/resources/db/rdb_modern/views/007-v_code_value_general-001.sql
@@ -0,0 +1,13 @@
+
+create or alter view dbo.v_code_value_general as 
+  select  v.code as CODE_VAL, 
+         v.code_short_desc_txt as CODE_DESC, 
+         v.code_system_cd as CODE_SYS_CD, 
+         v.code_system_desc_txt as CODE_SYS_CD_DESC,
+         v.effective_from_time as CODE_EFF_DT, 
+		 v.effective_to_time as CODE_END_DT
+		 ,c.cd
+   from dbo.v_codeset c join 
+   dbo.v_nrt_srte_code_Value_General v
+   on c.code_set_nm = v.code_set_nm 
+   ;
