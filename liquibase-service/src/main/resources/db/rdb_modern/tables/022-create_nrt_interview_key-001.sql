8fc86ddf56d617a55a0eb8c7d4c17460f65328e5
Joshua Mills <joshua.mills@cdw.com>
2024-12-30 15:58:53 -0500

    CNDE-2043: fix interview key tables, remove unnecessary select from postprocessing sproc

--- liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_key-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_key-001.sql
@@ -1,13 +1,15 @@
-DROP TABLE IF EXISTS dbo.nrt_interview_key;
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_interview_key' and xtype = 'U')
+    BEGIN
 
-CREATE TABLE dbo.nrt_interview_key (
-	d_interview_key bigint IDENTITY(1,1) NOT NULL,
-	interview_uid bigint NULL
-);
+        CREATE TABLE dbo.nrt_interview_key (
+            d_interview_key bigint IDENTITY (1,1) NOT NULL,
+            interview_uid   bigint                NULL
+        );
+        declare @max bigint;
+        select @max=max(d_interview_key)+1 from dbo.D_INTERVIEW ;
+        select @max;
+        if @max IS NULL   --check when max is returned as null
+            SET @max = 2; -- default to 2, as default record with key = 1 is not stored in D_INTERVIEW
+        DBCC CHECKIDENT ('dbo.nrt_interview_key', RESEED, @max);
 
-declare @max bigint;
-select @max=max(D_INTERVIEW_KEY)+1 from dbo.D_INTERVIEW;
-select @max;
-if @max IS NULL   --check when max is returned as null
-  SET @max = 1;
-DBCC CHECKIDENT ('dbo.nrt_interview_key', RESEED, @max);
+    END
--- liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_note_key-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/022-create_nrt_interview_note_key-001.sql
@@ -1,14 +1,16 @@
-DROP TABLE IF EXISTS dbo.nrt_interview_note_key;
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_interview_note_key' and xtype = 'U')
+    BEGIN
 
-CREATE TABLE dbo.nrt_interview_note_key (
-	d_interview_note_key bigint IDENTITY(1,1) NOT NULL,
-    d_interview_key bigint NOT NULL,
-	nbs_answer_uid bigint NULL
-);
+        CREATE TABLE dbo.nrt_interview_note_key (
+          d_interview_note_key bigint IDENTITY(1,1) NOT NULL,
+          d_interview_key bigint NOT NULL,
+          nbs_answer_uid bigint NULL
+        );
+        declare @max bigint;
+        select @max=max(d_interview_note_key)+1 from dbo.D_INTERVIEW_NOTE ;
+        select @max;
+        if @max IS NULL   --check when max is returned as null
+            SET @max = 2; -- default to 2, as default record with key = 1 is not stored in D_INTERVIEW_NOTE
+        DBCC CHECKIDENT ('dbo.nrt_interview_note_key', RESEED, @max);
 
-declare @max bigint;
-select @max=max(D_INTERVIEW_NOTE_KEY)+1 from dbo.D_INTERVIEW_NOTE;
-select @max;
-if @max IS NULL   --check when max is returned as null
-  SET @max = 1;
-DBCC CHECKIDENT ('dbo.nrt_interview_note_key', RESEED, @max);
+    END
