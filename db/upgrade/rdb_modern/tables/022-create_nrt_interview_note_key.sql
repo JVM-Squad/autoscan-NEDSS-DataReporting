4f55cde0416ef662b54a6041235a0f125146ebdc
Joshua Mills <joshua.mills@cdw.com>
2025-01-02 16:37:16 -0500

    CNDE-2043: update db/upgrade

--- db/upgrade/rdb_modern/tables/022-create_nrt_interview_note_key.sql
+++ db/upgrade/rdb_modern/tables/022-create_nrt_interview_note_key.sql
@@ -1,14 +1,18 @@
-DROP TABLE IF EXISTS dbo.nrt_interview_note_key;
+-- table is not dropped and recreated so as to stay consistent with the design of nrt_interview_key
 
-CREATE TABLE dbo.nrt_interview_note_key (
-	d_interview_note_key bigint IDENTITY(1,1) NOT NULL,
-    d_interview_key bigint NOT NULL,
-	nbs_answer_uid bigint NULL
-);
+IF NOT EXISTS (SELECT 1 FROM sysobjects WHERE name = 'nrt_interview_note_key' and xtype = 'U')
+    BEGIN
 
-declare @max bigint;
-select @max=max(D_INTERVIEW_NOTE_KEY)+1 from dbo.D_INTERVIEW_NOTE;
-select @max;
-if @max IS NULL   --check when max is returned as null
-  SET @max = 1;
-DBCC CHECKIDENT ('dbo.nrt_interview_note_key', RESEED, @max);
+        CREATE TABLE dbo.nrt_interview_note_key (
+          d_interview_note_key bigint IDENTITY(1,1) NOT NULL,
+          d_interview_key bigint NOT NULL,
+          nbs_answer_uid bigint NULL
+        );
+        declare @max bigint;
+        select @max=max(d_interview_note_key)+1 from dbo.D_INTERVIEW_NOTE ;
+        select @max;
+        if @max IS NULL   --check when max is returned as null
+            SET @max = 2; -- default to 2, as default record with key = 1 is not stored in D_INTERVIEW_NOTE
+        DBCC CHECKIDENT ('dbo.nrt_interview_note_key', RESEED, @max);
+
+    END
