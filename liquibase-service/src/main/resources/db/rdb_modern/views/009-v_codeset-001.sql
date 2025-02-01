85e0d7cf48dce28d8f5eb19503daf1ad466d4cf4
Upasana Pattnaik <129798209+upasanapattnaik-eq@users.noreply.github.com>
2025-01-31 19:28:57 -0500

    Cnde 2171 (#152)

--- liquibase-service/src/main/resources/db/rdb_modern/views/009-v_codeset-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/views/009-v_codeset-001.sql
@@ -1,12 +1,41 @@
-create or alter view dbo.v_codeset as
-select m.unique_cd          as CD,
-       RDB_table            as TBL_NM,
-       RDB_attribute        as COL_NM,
-       c.source_version_txt as CD_SYS_VER,
-       null                 as DATA_TYPE,
-       null                 as DATA_LENGTH,
-       t.srt_reference      as code_set_nm,
-       t.label              as cd_desc
-from dbo.v_nrt_srte_imrdbmapping m
-         left join dbo.v_nrt_srte_totalidm t on t.unique_cd = m.unique_cd
-         left join dbo.v_nrt_srte_codeset c on c.code_set_nm = t.srt_reference;
+CREATE OR ALTER VIEW dbo.v_codeset as
+WITH totalidm AS (
+    SELECT
+        unique_cd as  'CD',
+        SRT_reference as  'code_set_nm',
+        format as  'format',
+        label as  'cd_desc'
+    FROM dbo.v_nrt_srte_totalidm t
+),
+     ALL_CODESET as (
+         SELECT
+             LEFT(unique_cd, 7) as  'CD',
+             LEFT(RDB_table, 32) as  'TBL_NM',
+             RDB_attribute as  'COL_NM',
+             condition_cd as  'condition_cd'
+         FROM dbo.v_nrt_srte_imrdbmapping),
+     RDBCodeset AS
+         (
+             SELECT
+                 m.CD,
+                 m.TBL_NM,
+                 m.COL_NM,
+                 t.code_set_nm,
+                 LEFT(t.cd_desc,300) as cd_desc,
+                 NULL                 as DATA_TYPE,
+                 NULL                 as DATA_LENGTH
+             FROM totalidm t --A
+                      RIGHT JOIN ALL_CODESET m  --B
+                                 ON t.cd = m.cd
+         )
+SELECT
+    agg.CD,
+    agg.TBL_NM,
+    agg.COL_NM,
+    c.source_version_txt as CD_SYS_VER,
+    agg.DATA_TYPE,
+    agg.DATA_LENGTH,
+    NULLIF(c.code_set_nm,'') as code_set_nm,
+    COALESCE(c.code_set_desc_txt,agg.cd_desc)	as CD_DESC
+FROM RDBCodeset agg
+         LEFT JOIN dbo.v_nrt_srte_codeset c on c.code_set_nm = agg.code_set_nm;
--- liquibase-service/src/main/resources/db/rdb_modern/views/010-v_code_value_general-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/views/010-v_code_value_general-001.sql
@@ -1,3 +1,4 @@
+--There is no NULL row assigned to CODE_KEY =1
 create or alter view dbo.v_code_value_general as
 select v.code                 as CODE_VAL,
        v.code_short_desc_txt  as CODE_DESC,
@@ -5,7 +6,8 @@ select v.code                 as CODE_VAL,
        v.code_system_desc_txt as CODE_SYS_CD_DESC,
        v.effective_from_time  as CODE_EFF_DT,
        v.effective_to_time    as CODE_END_DT,
-       c.cd
+       c.cd,
+       ROW_NUMBER () over (ORDER BY c.cd) AS CODE_KEY
 from dbo.v_codeset c
          join
      dbo.v_nrt_srte_code_Value_General v
