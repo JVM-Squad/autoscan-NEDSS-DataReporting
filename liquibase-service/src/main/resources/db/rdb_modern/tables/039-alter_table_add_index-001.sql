bf3ecb208a4e9e71fcda1039a1decd6fbd485c33
Ravindran Seshadri <109151953+rseshadri99@users.noreply.github.com>
2025-02-17 16:48:52 -0500

    Fix for failing index creation - idx_interview_uid_rdb_column inn liquibase and consolidated script  (#171)
    Co-authored-by: Anand Logan <117679402+alog-enquizit@users.noreply.github.com>

--- liquibase-service/src/main/resources/db/rdb_modern/tables/039-alter_table_add_index-001.sql
+++ liquibase-service/src/main/resources/db/rdb_modern/tables/039-alter_table_add_index-001.sql
@@ -45,7 +45,8 @@ BEGIN
 ALTER TABLE dbo.nrt_contact_key ADD CONSTRAINT pk_d_contact_record_key PRIMARY KEY (d_contact_record_key);
 END
 
-IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = 'idx_contact_uid_rdb_column' AND object_id = OBJECT_ID('dbo.nrt_interview_answer'))
+
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = 'idx_interview_uid_rdb_column' AND object_id = OBJECT_ID('dbo.nrt_interview_answer'))
 BEGIN
 	CREATE INDEX idx_interview_uid_rdb_column ON dbo.nrt_interview_answer (interview_uid, rdb_column_nm);
 END
