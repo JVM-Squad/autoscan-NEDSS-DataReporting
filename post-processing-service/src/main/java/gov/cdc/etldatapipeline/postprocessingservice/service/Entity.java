ea889604179ed95f63b3a48630e507fc30e9b077
John Eipe <john.eipe@cdw.com>
2025-02-17 15:46:59 -0600

    Added post processing code for Hepatitis Case datamart

--- post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/Entity.java
+++ post-processing-service/src/main/java/gov/cdc/etldatapipeline/postprocessingservice/service/Entity.java
@@ -28,6 +28,7 @@ enum Entity {
     MEASLES_CASE(0, "Measles_Case", Constants.PHC_UID, "sp_measles_case_datamart_postprocessing"),
     CASE_LAB_DATAMART(0, "Case_Lab_Datamart", Constants.PHC_UID, "sp_case_lab_datamart_postprocessing"),
     BMIRD_CASE(0, "BMIRD_Case", Constants.PHC_UID, "sp_bmird_case_datamart_postprocessing"),
+    HEPATITIS_CASE(0, "Hepatitis_Case", Constants.PHC_UID, "sp_hepatitis_case_datamart_postprocessing"),
     UNKNOWN(-1, "unknown", "unknown_uid", "sp_nrt_unknown_postprocessing");
 
     private final int priority;
